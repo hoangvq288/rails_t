568cb216e4642e115fdbc7d3b7de22f7ea5cc694
Vũ Quốc Hoàng <hoangvq288@gmail.com>
2020-02-08 01:44:22 +0700

    Book model validation

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  # associations
-  it { is_expected.to belong_to(:user) }
+  
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:author) }
+  end
 
-  # columns
-  it { is_expected.to have_db_column(:name).of_type(:string) }
-  it { is_expected.to have_db_column(:author).of_type(:string) }
-  it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
-  it { is_expected.to have_db_column(:updated_at).of_type(:datetime)}
+  describe 'associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'columns' do
+    it { is_expected.to have_db_column(:name).of_type(:string) }
+    it { is_expected.to have_db_column(:author).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime)}
+  end
 
 end
